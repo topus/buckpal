95a38cf8979bcbcc466d91eb999234daf85400b8
topjohan <johan.top@conundra.eu>
2023-04-17 08:09:16 +0200

    remove unused imports

--- src/main/java/io/reflectoring/buckpal/account/application/port/in/SendMoneyUseCase.java
+++ src/main/java/io/reflectoring/buckpal/account/application/port/in/SendMoneyUseCase.java
@@ -1,9 +1,5 @@
 package io.reflectoring.buckpal.account.application.port.in;
 
-import io.reflectoring.buckpal.account.domain.Account.AccountId;
-import io.reflectoring.buckpal.account.domain.Money;
-import io.reflectoring.buckpal.common.SelfValidating;
-
 public interface SendMoneyUseCase {
 
 	boolean sendMoney(SendMoneyCommand command);
